bae1a30a2eb37507ba2dc336446be3d01481ca85
ThabatAcosta <91706609+ThabatAcosta@users.noreply.github.com>
2021-11-09 17:32:54 -0500

    Estlos tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className= "  bg-blue-600 w-full h-20 flex flex-col justify-start text-left px-6 py-6 ">
-            <h2 className=" font-mono font-semibold text-gray-900 ">Check The Weather</h2>
+        <div className= " bg-transparent w-full h-20 flex  justify-start text-left px-6 py-6 ">
+            <h2 className=" font-serif font-semibold text-gray-200 text-2xl">Check The Weather</h2>
         </div>
     )
 }
